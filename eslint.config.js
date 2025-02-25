8d0852daafd5ff1ca4c2f233fd617333f7a181c9
Jakob Edvardsson <jakob@edvardsson.tech>
2025-02-25 19:17:15 +0100

    fix: lint error

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'warn',
       '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
     },
   },
   eslintConfigPrettier,
